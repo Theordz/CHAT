bf7c08ff2ea693c01c779a15aed5f588b2a80af4
Theordz <167786293+Theordz@users.noreply.github.com>
2024-04-28 17:58:17 -0400

    Update GPT.py

--- GPT.py
+++ GPT.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 from PIL import Image
-image = Image.open('C:\Users\theor\Downloads\WOW.png')
+image = Image.open('Downloads\WOW.png')
 st.header("Ai Tool for Meal suggestions", divider='gray')
 st.subheader("By Spencer, Joel, and Theo")
 PROGRAM = "ON"
